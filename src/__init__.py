eea4e48e1ab603294b20e5f0753af269b78e7365
KonstantinosLougiakis <lougiokostas@gmail.com>
2024-04-16 17:53:31 +0300

    changes in init

--- src/__init__.py
+++ src/__init__.py
@@ -11,8 +11,7 @@ jwt = JWTManager(app)
 app.config["JWT_SECRET_KEY"] = "super secret and difficult to guess key"
 
 connect(
-    host="mongodb+srv://coding-factory:ufTXpw9bAEejrXfb@cluster0.okry00y.mongodb.net",
-        #  "mongodb+srv://lougiokostas:12345@cluster0.s2qq6ed.mongodb.net/"
+    host="mongodb+srv://cf-user1:ufTXpw9bAEejrXfb@cluster0.okry00y.mongodb.net/",
     db="Coding-Factory",
     alias="coding-factory",
 )
